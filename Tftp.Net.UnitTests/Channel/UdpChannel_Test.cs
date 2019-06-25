fbe9aae0996a2b856d88fce45e6d8e446ce923c8
Lukas Holota <l.holota@outlook.com>
2019-06-25 12:50:21 +0200

    .NET Core port

--- Tftp.Net.UnitTests/Channel/UdpChannel_Test.cs
+++ Tftp.Net.UnitTests/Channel/UdpChannel_Test.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using Tftp.Net.Channel;
 using System.Net.Sockets;
 using NUnit.Framework;
---
+++ Tftp.Net/Properties/InternalsVisibility.cs
@@ -0,0 +1,3 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Tftp.Net.UnitTests")]
